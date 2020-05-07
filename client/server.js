076a511795bf858fe7b193496bcb602b9beac345
gilbertVirgo <gilbertjvirgo@gmail.com>
2020-05-07 17:32:17 +0100

    Added test message inside server.js

--- client/server.js
+++ client/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
+	console.log("caught route!");
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.listen(4000, () => console.log("Listening on port 4000"));
